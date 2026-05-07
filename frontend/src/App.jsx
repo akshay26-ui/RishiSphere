9eb363b05719c82955bd629baf5d7c2bbac149c0
shreyanshagrawal <shreyanshagra123@gmail.com>
2026-05-08 04:47:33 +0530

    temperorily disabled protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,128 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-
-import Login from "./pages/Auth/Login/Login";
-
-import Signup from "./pages/Auth/Signup/Signup";
-
-import Calendar from "./pages/Calendar/Calendar";
-
-import SubmitEvent from "./pages/AddEvent/SubmitEvent";
-
-import Admin from "./pages/Admin/Admin";
-
-import ComingSoon from "./pages/ComingSoon/ComingSoon";
-
-import ProtectedRoute from "./routes/ProtectedRoute";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login/Login';
+import Signup from './pages/Auth/Signup/Signup';
+import Calendar from './pages/Calendar/Calendar';
+import SubmitEvent from './pages/AddEvent/SubmitEvent';
+import Admin from './pages/Admin/Admin';
+import ComingSoon from './pages/ComingSoon/ComingSoon';
 
 export default function App() {
     return (
         <Routes>
-            {/* Public Routes */}
-
             <Route path="/" element={<Home />} />
-
             <Route path="/login" element={<Login />} />
-
             <Route path="/signup" element={<Signup />} />
-
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/submit-event" element={<SubmitEvent />} />
+            <Route path="/admin" element={<Admin />} />
             <Route path="/coming-soon" element={<ComingSoon />} />
-
-            {/* Protected Routes */}
-
-            <Route
-                path="/calendar"
-                element={
-                    <ProtectedRoute>
-                        <Calendar />
-                    </ProtectedRoute>
-                }
-            />
-
-            <Route
-                path="/submit-event"
-                element={
-                    <ProtectedRoute>
-                        <SubmitEvent />
-                    </ProtectedRoute>
-                }
-            />
-
-            {/* Admin Only */}
-
-            <Route
-                path="/admin"
-                element={
-                    <ProtectedRoute allowedRoles={["admin"]}>
-                        <Admin />
-                    </ProtectedRoute>
-                }
-            />
         </Routes>
     );
 }
+
+// import { Routes, Route }
+//     from "react-router-dom";
+//
+// import Home
+//     from "./pages/Home/Home";
+//
+// import Login
+//     from "./pages/Auth/Login/Login";
+//
+// import Signup
+//     from "./pages/Auth/Signup/Signup";
+//
+// import Calendar
+//     from "./pages/Calendar/Calendar";
+//
+// import SubmitEvent
+//     from "./pages/AddEvent/SubmitEvent";
+//
+// import Admin
+//     from "./pages/Admin/Admin";
+//
+// import ComingSoon
+//     from "./pages/ComingSoon/ComingSoon";
+//
+// import ProtectedRoute
+//     from "./routes/ProtectedRoute";
+//
+// export default function App() {
+//
+//     return (
+//
+//         <Routes>
+//
+//             {/* Public Routes */}
+//
+//             <Route
+//                 path="/"
+//                 element={<Home />}
+//             />
+//
+//             <Route
+//                 path="/login"
+//                 element={<Login />}
+//             />
+//
+//             <Route
+//                 path="/signup"
+//                 element={<Signup />}
+//             />
+//
+//             <Route
+//                 path="/coming-soon"
+//                 element={<ComingSoon />}
+//             />
+//
+//             {/* Protected Routes */}
+//
+//             <Route
+//                 path="/calendar"
+//
+//                 element={
+//
+//                     <ProtectedRoute>
+//
+//                         <Calendar />
+//
+//                     </ProtectedRoute>
+//                 }
+//             />
+//
+//             <Route
+//                 path="/submit-event"
+//
+//                 element={
+//
+//                     <ProtectedRoute>
+//
+//                         <SubmitEvent />
+//
+//                     </ProtectedRoute>
+//                 }
+//             />
+//
+//             {/* Admin Only */}
+//
+//             <Route
+//                 path="/admin"
+//
+//                 element={
+//
+//                     <ProtectedRoute
+//                         allowedRoles={[
+//                             "admin",
+//                         ]}
+//                     >
+//
+//                         <Admin />
+//
+//                     </ProtectedRoute>
+//                 }
+//             />
+//
+//         </Routes>
+//     );
+// }
